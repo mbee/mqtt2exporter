8f5d40a654741b7c28c89ba1741bad5cf9dbf2dc
Marc Bee <marc.bee@gmail.com>
2020-10-20 08:02:41 +0200

    hopefully latest version 1.0.0 :)
    Former-commit-id: b585ce857cb395caecaf0713ef8705887ff6752b

--- make.sh
+++ make.sh
@@ -9,17 +9,17 @@ rm -rf release/$VERSION
 
 echo build release/$VERSION/linux/arm
 mkdir -p release/$VERSION/linux/arm
-GOOS=linux GOARCH=arm GOARM=5 go build -o $NAME ./src
+GOOS=linux GOARCH=arm GOARM=5 go build $NAME.go
 mv $NAME release/$VERSION/linux/arm
 
 echo build release/$VERSION/linux/amd64
 mkdir -p release/$VERSION/linux/amd64
-GOOS=linux GOARCH=amd64 go build -o $NAME ./src
+GOOS=linux GOARCH=amd64 go build $NAME.go
 mv $NAME release/$VERSION/linux/amd64
 
 echo build release/$VERSION/windows/amd64
 mkdir -p release/$VERSION/windows/amd64
-GOOS=windows GOARCH=amd64 go build -o $NAME.exe ./src
+GOOS=windows GOARCH=amd64 go build $NAME.go
 mv $NAME.exe release/$VERSION/windows/amd64
 
 echo tar devices yml files
